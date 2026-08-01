cff727f49264e061efb879f2048b9d18e5862c45
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-08-01 12:51:37 +0530

    Add Student class and update app.js to utilize it; modify index.html to load app.js as a module

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-import {multiply,sum} from "./sum.js";
+import {multiply,sum,name} from "./sum.js";
+
+import Student from "./Student.js"
 
 let numSum = sum(10,20);
 console.log(numSum);
 
 let numMul = multiply(5,2);
 console.log(numMul);
+
+console.log(name);
+
+
+
+let student = new Student("saman",12,"walana");
+
+console.log(student);
